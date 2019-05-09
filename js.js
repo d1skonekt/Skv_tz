b56fc929081f0d02f61c17d9ce6110881e9904ad
d1skonekt <47295279+d1skonekt@users.noreply.github.com>
2019-05-09 22:02:42 +0300

    up array[Obj]

--- js.js
+++ js.js
@@ -14,7 +14,7 @@ let Chess = {
         this.setBoardSize();
     },
     setBoardSize: function () {
-        if (document.body.offsetWidth >= document.body.offsetHeight) {
+        if (document.body.offsetWidth >= document.body.offsetHeight) {  // условие для всегда правильного квадрата
             this.board.domElement.style.width = '90vh';
             this.board.domElement.style.height = '90vh';
         } else {
@@ -24,12 +24,15 @@ let Chess = {
         this.defaultSize = this.board.domElement.offsetWidth;
     },
     createCells: function () {  // создание ячеек поля + добавление позиции X,Y + добавление классов для окраса;
-        this.board.cells = [];
+        this.board.cells = []; //  массив обьектов с параметрами позиции ячейки (x,y) + domElement
         let x = 1, y = 8;
         for (let i = 0; i < 64; i++) {
-            this.board.cells[i] = document.createElement('div');
-            this.board.domElement.appendChild(this.board.cells[i]);
-            this.board.cells[i].classList.add('chess_cell');
+            this.board.cells[i] = {
+                id: i + '',
+                domElement: document.createElement('div')
+            }
+            this.board.domElement.appendChild(this.board.cells[i].domElement);
+            this.board.cells[i].domElement.classList.add('chess_cell');
 
             if (x > 8) {
                 x = 1;
@@ -39,9 +42,9 @@ let Chess = {
             this.board.cells[i].posY = y;
             x++;
             if (((i % 2 == 0) && (y % 2 == 0)) || ((i % 2 != 0) && (y % 2 != 0))) {
-                this.board.cells[i].classList.add('yellow_bg');
+                this.board.cells[i].domElement.classList.add('yellow_bg');
             } else {
-                this.board.cells[i].classList.add('brown_bg');
+                this.board.cells[i].domElement.classList.add('brown_bg');
             }
         }
     },
@@ -52,32 +55,32 @@ let Chess = {
         this.horse.domElement.setAttribute('draggable', true)
         this.board.domElement.appendChild(this.horse.domElement);
 
-        this.randomPosition = Math.round(Math.random() * 63); // рандомизируем первое место при загрузке страницы для коня
+        this.randomPosition = Math.round(Math.random() * 63);   // рандомизируем первое место при загрузке страницы для коня
 
-        this.setHorse();
+        this.setHorseParams();
 
     },
-    setHorse: function () {
-        this.horse.domElement.style.width = (this.defaultSize * 0.124) + 'px';
+    setHorseParams: function () {
+        this.horse.domElement.style.width = (this.defaultSize * 0.124) + 'px';  // определение ширины/высоты коня
         this.horse.domElement.style.height = this.horse.domElement.style.width;
 
-        this.horse.posX = this.board.cells[this.randomPosition].getBoundingClientRect().x;
-        this.horse.posY = this.board.cells[this.randomPosition].getBoundingClientRect().y;
+        this.horse.posX = this.board.cells[this.randomPosition].domElement.getBoundingClientRect().x;  //присвоение позициям X,Y первоначальное рандомное значение
+        this.horse.posY = this.board.cells[this.randomPosition].domElement.getBoundingClientRect().y;
 
         this.moveHorse();
 
     },
-    setListeners: function () {
+    setListeners: function () {     //перерисовка поля и коня при изменении размеров
         document.body.onresize = () => {
             this.setBoardSize();
-            this.setHorse();
+            this.setHorseParams();
         }
     },
     moveHorse: function () {
-        this.horse.amendmentY = (document.body.offsetHeight - this.board.domElement.offsetHeight) / 2
+        this.horse.amendmentY = (document.body.offsetHeight - this.board.domElement.offsetHeight) / 2 //определение поправок для выставления коня по центру
         this.horse.amendmentX = (document.body.offsetWidth - this.board.domElement.offsetWidth) / 2
 
-        this.horse.domElement.style.top = (this.horse.posY - this.horse.amendmentY) + 'px';
+        this.horse.domElement.style.top = (this.horse.posY - this.horse.amendmentY) + 'px';     // перемещение коня на позицию с учетом поправки
         this.horse.domElement.style.left = (this.horse.posX - this.horse.amendmentX) + 'px';
     }
 }
@@ -87,6 +90,7 @@ document.addEventListener('DOMContentLoaded', function () {
     Chess.init();
     console.log(Chess.board);
     console.log(Chess.horse);
+    console.log(Chess.board.cells);
 }, false);
 
 
