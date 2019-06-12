c052870a86b57fb48252ffe2b268dcd62334fb72
d1skonekt <47295279+d1skonekt@users.noreply.github.com>
2019-06-12 23:25:30 +0300

    end

--- js/js.js
+++ js/js.js
@@ -27,12 +27,6 @@ let Chess = {
 
   setBoardSize: function () {
     // условие для всегда правильного квадрата
-    if (this.mobile) {
-      // код для мобильных устройств (для правки смещения коня)
-      window.scrollTo(0,1);
-      // document.body.style.position = 'fixed';
-    }
-    // код для других устройств
     if (document.body.offsetWidth >= document.body.offsetHeight) {
       this.board.domElement.style.width = '90vh';
       this.board.domElement.style.height = '90vh';
