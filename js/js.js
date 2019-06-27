3600dabedcd31845dd7b39fbecab6123c68b1e63
d1skonekt <47295279+d1skonekt@users.noreply.github.com>
2019-06-28 00:22:43 +0300

    Update js.js

--- js/js.js
+++ js/js.js
@@ -152,7 +152,7 @@ let Chess = {
 
     //Клик мышки по коню (grad & drop)
     this.horse.domElement.addEventListener(dragStart, (event) => {
-      document.body.style.overscrollBehavior = 'none';
+      // document.body.style.overscrollBehavior = 'none';
       if (event.target.classList.contains('horse')) {
         this.horse.isDrag = true;
         //удаляем подсветку если нажали на коня и готовы передвинуть его
@@ -179,7 +179,7 @@ let Chess = {
 
     // окончание движение мышки и дроп коня с  учетом того , что начался Драг
     this.horse.domElement.addEventListener(dropEnd, () => {
-      document.body.style.overscrollBehavior = 'auto';
+      // document.body.style.overscrollBehavior = 'auto';
       if (this.horse.isDrag) {
         // переопределение позиции коня для корректной подсветки и растановке на поле
         this.board.cells.forEach(element => {
