282275ddd9f526c57a397baa968d86d0dbf6b5ed
d1skonekt <47295279+d1skonekt@users.noreply.github.com>
2019-07-26 18:21:18 +0300

    check Fullscreen

--- js/js.js
+++ js/js.js
@@ -304,6 +304,13 @@ let Chess = {
       }
     });
 
+    //проверка на изменения фулскрина если выход из режима производится стандартными методами
+    document.body.addEventListener("webkitfullscreenchange", function () {
+      // упрощение записи
+      if (this.fullscreenInfo) this.fullscreenInfo = false
+      else this.fullscreenInfo = true;
+    });
+
   },
 
 
